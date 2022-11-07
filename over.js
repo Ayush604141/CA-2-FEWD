30b29662bd28727bb8b02a73af404df9d3ea308e
Ayush604141 <112609589+Ayush604141@users.noreply.github.com>
2022-11-07 15:52:20 +0530

    Update over.js

--- over.js
+++ over.js
@@ -2,19 +2,19 @@ const message = document.getElementById("message")
 var score = document.getElementById("score")
 const replayButton = document.getElementById("replay")
 var audio = new Audio("./assets/music/EndPage.mp3")
-window.onload = audio.play();
-if(score >= 100)
-{
-    message.innerText = "You Played Well"
-}
-else{
-    message.innerText = "Sorry Pal, Better Luck Next Time"
-}
-
+End_game = localStorage.getItem("Name");
+audio.play();
 var Score_update = localStorage.getItem("Score")
 var Word_lost = localStorage.getItem("Word")
-var Player_name = localStorage.getItem("Name")
-score.innerText = Score_update;
+if(Score_update >= 100)
+{
+    message.innerText = "You Played Well";
+}
+else{
+    message.innerText = "Sorry Pal, You didn't guess", Word_lost
+}
+
+score.innerText = End_game ,", You Scored", Score_update;
 
 replayButton.onclick = replay;
 
